6e907c3d3164bcbcaa1ba01b9f7ab75eeef55a72
Angelo Luppino <angelo.luppino@spcapitaliq.com>
2016-04-05 15:55:55 -0400

    Convert PUT /todos/:id to sequelize

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/todos', function(req, res) {
 		where: where
 	}).then(function(todos) {
 		res.json(todos);
-	}).catch(function(e) {
+	}, function() {
 		res.status(500).send();
 	});
 });
@@ -50,7 +50,7 @@ app.get('/todos/:id', function(req, res) {
 		} else {
 			res.status(404).send();
 		}
-	}).catch(function(e) {
+	}, function() {
 		res.status(500).send();
 	});
 });
@@ -61,7 +61,7 @@ app.post('/todos', function(req, res) {
 
 	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	}).catch(function(e) {
+	}, function(e) {
 		res.status(400).json(e);
 	});
 });
@@ -80,7 +80,7 @@ app.delete('/todos/:id', function(req, res) {
 		} else {
 			res.status(204).send();
 		}
-	}).catch(function(e) {
+	}, function() {
 		res.status(500).send()
 	});
 });
@@ -88,28 +88,30 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		res.status(404).json({"error": "No todo found with id " + todoId});
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
-
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			return todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function() {
+		res.status(500).send();
+	});
 });
 
 db.sequelize.sync().then(function() {
